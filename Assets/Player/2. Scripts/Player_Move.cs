c7c3c1c51bd3b242933b76b5f9452dfad90aef4c
kh6206 <jkh6206@naver.com>
2022-11-02 14:00:47 +0900

    player
    11/2일자 작업 분량 더블점프, 무기 콜라이더

--- Assets/Player/2. Scripts/Player_Move.cs
+++ Assets/Player/2. Scripts/Player_Move.cs
@@ -9,8 +9,20 @@ public class Player_Move : MonoBehaviour
     public float jumpPower;
     Rigidbody2D Player_rigid;
     Transform Player_tr;
+    bool doubleJumpState = false;
+    bool isGround = true;
+
+    // 대쉬 변수
+    private bool canDash = true;
+    private bool isDash;
+    private float dashPower = 20.0f;
+    private float dashTime = 0.2f;
+    private float dashCooldown = 1f;
+
+
     
-    public int prHp = 100;
+    
+    public int prHp = 100; //플레이어 체력
     SpriteRenderer sprite;
     // Start is called before the first frame update
     void Start()
@@ -20,11 +32,25 @@ public class Player_Move : MonoBehaviour
         
         sprite = GetComponent<SpriteRenderer>();
     }
+
+
     void Jump() 
     {
-        if (Input.GetKeyDown(KeyCode.C))
+        if (Player_rigid.velocity.y == 0)
+            isGround = true;
+        else
+            isGround = false;
+        if (isGround)
+            doubleJumpState = true;
+
+
+        if (isGround && Input.GetKeyDown(KeyCode.C))
         {
             Player_rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+        }else if (doubleJumpState && Input.GetKeyDown(KeyCode.C))
+        {
+            Player_rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            doubleJumpState = false;
         }
     }
     
@@ -47,6 +73,12 @@ public class Player_Move : MonoBehaviour
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
         transform.position += speed * Time.deltaTime * new Vector3(h, v, 0);
+
+        if (Input.GetKeyDown(KeyCode.Z)) //플레이어 대쉬
+        {
+
+        }
+        
     }
 
     // Update is called once per frame
