068f6727f64c41b0a4b2d394ae9658ad98c614b9
kh6206 <jkh6206@naver.com>
2023-02-08 10:01:13 +0900

    player

--- Assets/2. Player/02. Scripts/Spear/Player_Anim_Spear.cs
+++ Assets/2. Player/02. Scripts/Spear/Player_Anim_Spear.cs
@@ -2,28 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player_Anim_Spear : MonoBehaviour
+public class Player_Anim_Spear : Player_Attack
 {
     //public GameObject Spear_skill; //스킬
     public Transform Skill_pos;
     Animator Spear_anim;
 
-    public Transform pos;
-    public Vector2 player_boxSize;
+    //public Transform pos;
+    //public Vector2 player_boxSize;
     public GameObject enemy;
 
     //넉백
     public float Knockback_speed = 3;
-    protected bool isKnockback;
-    public float Kb_delayTime = 2f;
+    //protected bool isKnockback;
+    //public float Kb_delayTime = 2f;
 
-    protected float Kb_timer = 0f;
+    //protected float Kb_timer = 0f;
 
     //스킬
     public GameObject Spear_skill;
 
     //게이지
-    public float Skill_gauge = 0;
+    //public float Skill_gauge = 0;
 
     GameObject[] Enemy_Test;
 
@@ -64,7 +64,7 @@ public class Player_Anim_Spear : MonoBehaviour
     void Attack()
     {
         
-
+        /*
         if (Input.GetKeyDown(KeyCode.X))
         {
             Collider2D[] collider2Ds = Physics2D.OverlapBoxAll(pos.position, player_boxSize, 0); //박스안에 놓여진 모든 오브젝트들을 collider2d[] 배열에 담음
@@ -110,6 +110,8 @@ public class Player_Anim_Spear : MonoBehaviour
             //Vector2 dir = (transform.position - Enemy.transform.position).normalized;
             //player_rigid.AddForce(dir * str, ForceMode2D.Impulse);
         }
+        */
+
         if (Input.GetKeyDown(KeyCode.A) && Skill_gauge >= 100 ) //스킬게이지가 100이고 A키를 누르면
         {
             
