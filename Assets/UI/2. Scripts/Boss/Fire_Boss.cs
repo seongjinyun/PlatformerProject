7b2125a472c2127a6b30d90aa63d5bf49ebf12be
SangMyeong5426 <tvn2398@gmail.com>
2023-03-13 10:27:49 +0900

    FireBoss Com

--- Assets/UI/2. Scripts/Boss/Fire_Boss.cs
+++ Assets/UI/2. Scripts/Boss/Fire_Boss.cs
@@ -24,19 +24,30 @@ public class Fire_Boss : Basic_Boss
     {
         base.LookPlayer();//플레이어 방향 바라보기
         isDash = true;
+        DashPos.SetActive(true);
         yield return new WaitForSeconds(1.5f); //패턴 피할 시간
         transform.position = Vector2.MoveTowards(transform.position, DashDir.position, speed * Time.deltaTime); // 보스전방에 DashDir라는 빈 오브젝트 생성해서 추적(전방으로 돌진하게끔) 타겟 포지션으로 하면 이상하게 안됨
         yield return new WaitForSeconds(2.5f);
         isDash = false;
         anim.SetBool("Dash", false);
+        DashPos.SetActive(false);
         StartCoroutine(RandomPattern());
     }
-
+    IEnumerator TeleAttack()
+    {
+        transform.position = Target.transform.position;
+        yield return new WaitForSeconds(0.5f);
+        base.LookPlayer();
+        anim.SetBool("Attack", true);
+        yield return new WaitForSeconds(1.5f);
+        anim.SetBool("Attack", false);
+        StartCoroutine(RandomPattern());
+    }
     IEnumerator RandomPattern()
     {
         yield return new WaitForSeconds(2.0f); //패턴 사이에 나오는 경직 시간
 
-        int ranPattern = Random.Range(0, 3);
+        int ranPattern = Random.Range(0, 4);
         switch (ranPattern)
         {
             case 0:
@@ -48,6 +59,9 @@ public class Fire_Boss : Basic_Boss
             case 2:
                 StartCoroutine(FireMeteor());
                 break;
+            case 3:
+                StartCoroutine(TeleAttack());
+                break;
         }
     }
     IEnumerator FireBreath()
@@ -55,7 +69,7 @@ public class Fire_Boss : Basic_Boss
         LookPlayer();
 
         anim.SetBool("Breath", true);
-        yield return new WaitForSeconds(0.4f);
+        yield return new WaitForSeconds(0.3f);
         GameObject Breath = Instantiate(BreathPrepab, BreathPos.position, BreathPos.rotation);
         yield return new WaitForSeconds(2.0f);
         anim.SetBool("Breath", false);
