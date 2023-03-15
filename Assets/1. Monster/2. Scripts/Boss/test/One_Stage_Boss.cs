7f2749f3a7d7722a32f9f5b9505b7d33ce89809d
kh6206 <jkh6206@naver.com>
2023-03-16 01:18:00 +0900

    Earth_boss

--- Assets/1. Monster/2. Scripts/Boss/test/One_Stage_Boss.cs
+++ Assets/1. Monster/2. Scripts/Boss/test/One_Stage_Boss.cs
@@ -13,13 +13,14 @@ public class One_Stage_Boss : Basic_Boss
     {
         base.Start();
         StartCoroutine(RandomPattern());
+        Player_Head = GameObject.FindGameObjectWithTag("Monster_Skill_Pos").GetComponent<Transform>();
     }
 
     protected override void Update()
     {
         base.Update();
         
-        Attack();
+        
     }
 
     IEnumerator RandomPattern()
@@ -60,7 +61,7 @@ public class One_Stage_Boss : Basic_Boss
         transform.position = Vector2.MoveTowards(transform.position, DashDir.position, speed * Time.deltaTime); // 보스전방에 DashDir라는 빈 오브젝트 생성해서 추적(전방으로 돌진하게끔) 타겟 포지션으로 하면 이상하게 안됨
         yield return new WaitForSeconds(2.5f);
         isDash = false;
-        anim.SetBool("Dash", false);
+        anim.SetBool("Run", false);
         DashPos.SetActive(false);
         StartCoroutine(RandomPattern());
     }
@@ -70,28 +71,16 @@ public class One_Stage_Boss : Basic_Boss
         base.LookPlayer();
         anim.SetBool("Attack_2", true); // 애니메이션 실행
         yield return new WaitForSeconds(1f); // 1초뒤에
-        GameObject Skill_1_pos = Instantiate(Pre_EarthGrow, Target.position, Quaternion.Euler(0, 0, 0)); // 플레이어 위치에 준비 스킬뜨고
+        GameObject Skill_1_pos = Instantiate(Pre_EarthGrow, Player_Head.position, Quaternion.Euler(0, 0, 0)); // 플레이어 위치에 준비 스킬뜨고
         yield return new WaitForSeconds(1f); // 1초뒤에
         Destroy(Skill_1_pos); // 준비 스킬 삭제
-        GameObject Skill_1 = Instantiate(EarthGrowSkill, Target.position, Quaternion.Euler(0, 0, 0)); // 플레이어 위치에 스킬 뜸
+        GameObject Skill_1 = Instantiate(EarthGrowSkill, Player_Head.position, Quaternion.Euler(0, 0, 0)); // 플레이어 위치에 스킬 뜸
         Destroy(Skill_1, 1f); // 1초뒤에 삭제
         anim.SetBool("Attack_2", false); // 애니메이션 Idle로
+        StartCoroutine(RandomPattern());
 
     }
-    protected virtual void Attack()
-    {
-        if (Attack_State)
-        {
-            anim.SetTrigger("Attack");
-            anim.SetBool("Run", false);
-        }
-        else
-        {
-            
-        }
-
-
-    }
+   
     private void OnDrawGizmos() // 추적 범위
     {
         Gizmos.color = Color.red;
