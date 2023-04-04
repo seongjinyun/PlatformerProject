4960e294321f343a8a644d319894d120dd516f15
seongjinyun <100853424+seongjinyun@users.noreply.github.com>
2023-04-04 15:35:38 +0900

    0404 boss 수정

--- Assets/4. UI/2. Scripts/Boss/Basic_Boss.cs
+++ Assets/4. UI/2. Scripts/Boss/Basic_Boss.cs
@@ -32,14 +32,16 @@ public class Basic_Boss : Monster_Stats
     public Transform Target, DashDir;
 
     public AllUnits.Unit player_Hp;
+    Transform Player;
 
     protected override void Start()
     {
         base.Start();
         //anim = Child_anim.GetComponent<Animator>();
-        Target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        Target = GameObject.FindGameObjectWithTag("Player_Hit").GetComponent<Transform>();
+        Player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
         DashDir = GameObject.FindGameObjectWithTag("Target").GetComponent<Transform>();
-        player_Hp = Target.GetComponent<AllUnits.Unit>();
+        player_Hp = Player.GetComponent<AllUnits.Unit>();
     }
 
     // Update is called once per frame
@@ -50,7 +52,7 @@ public class Basic_Boss : Monster_Stats
 
     public void LookPlayer()
     {
-        if (transform.position.x < Target.transform.position.x)
+        if (transform.position.x < Player.transform.position.x)
         {
             transform.rotation = Quaternion.Euler(0, 0, 0);
         }
@@ -60,7 +62,7 @@ public class Basic_Boss : Monster_Stats
         }
 
     }
-    protected virtual void Collider() // 텔포 공격 플레이어의 체력을 깎음
+    protected virtual void Collider_Atk() // 텔포 공격 플레이어의 체력을 깎음
     {
         Collider2D collider2d = Physics2D.OverlapCircle(Attack_Pos.position, Attack_Radius, P_Layer); 
         // Attack_Pos 오브젝트로 포지션 지정, Attack_Radius 공격 범위를 지정
@@ -79,6 +81,22 @@ public class Basic_Boss : Monster_Stats
                 Debug.Log("PlayerHP =" + (player_Hp.currentHealth - Dash_Damage));
                 player_Hp.TakeDamage(Dash_Damage);
             }
+        }
+    }
+    protected virtual void Collider_Dash() // 텔포 공격 플레이어의 체력을 깎음
+    {
+        Collider2D collider2d = Physics2D.OverlapCircle(Attack_Pos.position, Attack_Radius, P_Layer);
+        // Attack_Pos 오브젝트로 포지션 지정, Attack_Radius 공격 범위를 지정
+        // 인스펙터에서 P_Layer에 Player 레이어로 지정
+
+        if (collider2d)
+        {
+            if (isDash) // 대쉬 공격 - 달리는 애니메이션 이벤트에 맨 앞에 Collider()함수 추가 + 달리는 애니메이션 스피드 0.7로 지정
+            {
+                Debug.Log("PlayerHP =" + (player_Hp.currentHealth - Dash_Damage));
+                player_Hp.TakeDamage(Dash_Damage);
+            }
+
         }
     }
     private void OnDrawGizmosSelected()
