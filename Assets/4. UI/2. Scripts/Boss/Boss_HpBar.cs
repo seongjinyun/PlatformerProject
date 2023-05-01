b1a098b46f677b169ded624f85e13fbd48a725f2
SangMyeong5426 <tvn2398@gmail.com>
2023-05-01 17:37:30 +0900

    dd
    dd

---
+++ Assets/4. UI/2. Scripts/Boss/Boss_HpBar.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Boss_HpBar : MonoBehaviour
+{
+    Monster_Stats stat;
+    public GameObject Boss;
+    public Slider BossHpbar;
+    public float maxhp, curhp;
+    void Start()
+    {
+        Boss = GameObject.FindWithTag("Monster");
+
+        stat = Boss.GetComponent<Monster_Stats>();
+
+        maxhp = stat.Monster_hpMax;
+        curhp = stat.Monster_currentHp;
+        
+        BossHpbar.value = (float)curhp / (float)maxhp;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        curhp = stat.Monster_currentHp;
+
+        BossHp();
+    }
+
+    private void BossHp()
+    {
+        BossHpbar.value = (float)curhp / (float)maxhp;
+    }
+}
