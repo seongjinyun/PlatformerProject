e43177d982ca0058c59ef630a3cd1a061eb2d54a
kh6206 <jkh6206@naver.com>
2023-05-09 00:34:56 +0900

    d

--- Assets/2. Player/02. Scripts/Sword/Player_Anim.cs
+++ Assets/2. Player/02. Scripts/Sword/Player_Anim.cs
@@ -56,7 +56,7 @@ public class Player_Anim : Player_Attack
     {
 
         
-        if (Input.GetKeyDown(KeyCode.A) /*&& Skill_gauge >= 100*/) //스킬게이지가 100이고 A키를 누르면
+        if (Input.GetKeyDown(KeyCode.A) && Skill_gauge >= 100) //스킬게이지가 100이고 A키를 누르면
         {
 
             if (self.transform.rotation.eulerAngles.y > 0) // 오른쪽으로 스킬
