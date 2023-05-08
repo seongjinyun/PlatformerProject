f96849510b0284cae55b7e3f33e9163f1bc988b2
SangMyeong5426 <tvn2398@gmail.com>
2023-05-09 00:28:51 +0900

    dd
    dd

---
+++ Assets/dddddd.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dddddd : MonoBehaviour
+{
+    Mode_Select mode;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mode.Hard = true;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
