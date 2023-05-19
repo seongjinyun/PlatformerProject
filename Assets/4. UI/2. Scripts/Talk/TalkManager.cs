ec413868c798e2a5581e2cc8a66107c2c238c1ef
SangMyeong5426 <tvn2398@gmail.com>
2023-05-19 11:04:05 +0900

    ddd
    dddd

--- Assets/4. UI/2. Scripts/Talk/TalkManager.cs
+++ Assets/4. UI/2. Scripts/Talk/TalkManager.cs
@@ -72,7 +72,7 @@ public class TalkManager : MonoBehaviour
             StartCoroutine(ActTalk3());
         }
 
-        if (DataNum == 14)
+        if (DataNum == 15)
         {
             PannelOff();
         }
@@ -85,7 +85,7 @@ public class TalkManager : MonoBehaviour
             PannelOff3();
         }
 
-        if (DataNum == 2 || DataNum == 5 || DataNum == 9 || DataNum == 11 || DataNum == 13 || DataNum2 == 2)
+        if (DataNum == 2 || DataNum == 4 || DataNum == 6 || DataNum == 9 || DataNum == 12 || DataNum == 14)
         {
             Portrait.sprite = CharSprite;
         }
@@ -111,21 +111,22 @@ public class TalkManager : MonoBehaviour
 
     void GenerateData()
     {
-        talkData.Add(1, "크레아토르? 내가 왜 이런 곳에 온거지?");
-        talkData.Add(2, "당신은 용사로 선택 받아 이세계로 오게 됐어요.");
-        talkData.Add(3, "이 세계에는 지구와는 달리 몬스터들이 도사리고있어요. 그렇기 때문에 지금부터 당신에게 몬스터들과 싸울 수 있는 힘을 어떻게 사용해야하는지 알려주려고 해요.");
-        talkData.Add(4, "아니 잠깐.. 몬스터? 그게 무슨말이에요!!");
-        talkData.Add(5, "사실.. 이 세계는 정령들과 인간들이 조화롭게 살고 있었어요. 하지만 봉인 되어있던 마왕이 그 안에서 모종의 계략을 꾸몄고 결국, 봉인이 깨져버리고 말았어요.");
-        talkData.Add(6, "그렇게 봉인의 큰 축이던 정령왕은 타락하게 되면서 일반 정령들은 사람들을 공격하는 몬스터가 되어버리고 말았죠.");
-        talkData.Add(7, "당신의 역할은 정령들을 정화시켜 본래대로 돌려놓고, 정령의 파편을 모아 마왕을 봉인해야 해요.");
-        talkData.Add(8, "제가 무슨 수로요? 그리고 제가 왜 해야해요! 저 말고 다른 사람들도 많잖아요!");
-        talkData.Add(9, "당신은 선택받은 용사. 다른 사람들은 할 수 없는 일이라 그래요. 만약, 도와주신다면 당신을 지구로 돌려보내드리고 소원을 들어줄게요.");
-        talkData.Add(10, "거부하면 어떻게 되죠?");
-        talkData.Add(11, "정말 안타깝지만 당신은 순리대로 죽게 되겠죠…");
-        talkData.Add(12, "...알겠어요 당신 말대로 하죠");
-        talkData.Add(13, "");
+        talkData.Add(1, "크레아토르? 제가 왜 이런 곳에 오게 된거죠?");
+        talkData.Add(2, "당신은 과로로 인해 죽게 됐고, 크레아토르로 환생하게 됐어요.");
+        talkData.Add(3, "그럴리가... 전 분명 야근을...");
+        talkData.Add(4, "크레아토르에는 지구와는 달리 마왕이 존재하는데 당신은 마왕의 하수인을 무찌르고 마왕을 재봉인하기 위해 선택된 용사예요.");
+        talkData.Add(5, "아니 잠깐…. 마왕? 그게 무슨 말이에요?");
+        talkData.Add(6, "봉인되어있던 마왕이 계략을 꾸몄고 봉인의 축인 정령왕이 타락하게 되면서 봉인이 해제되고 있어요.");
+        talkData.Add(7, "당신의 역할은 마왕의 하수인을 무찌르고 타락한 정령왕을 정화해 본래대로 되돌려놓고, 정령의 파편을 모아 봉인을 고쳐야 해요.");
+        talkData.Add(8, "꼭 제가 해야 하는 거예요? 제가 무슨 수로 마왕의 하수인을 무찔러요?");
+        talkData.Add(9, "선택받은 용사만이 할 수 있는 일이라 그래요.");
+        talkData.Add(10, "만약, 도와주신다면 당신을 지구로 돌려보내 드리고 소원을 들어줄게요.");
+        talkData.Add(11, "거부하면 어떻게 되죠?");
+        talkData.Add(12, "정말 안타깝지만, 당신은 순리대로 죽게 되겠죠...");
+        talkData.Add(13, "...알겠어요. 당신 말대로 할게요");
+        talkData.Add(14, "");
         
-        talkData2.Add(1, "뭐, 조금은요. 이제.. 뭘 더 하면 되죠?");
+        talkData2.Add(1, "뭐, 조금은요. 이제.. 뭘 더 하면 되죠?");                                                                                                                                                                 
         talkData2.Add(2, "이제 옆에 보이는 포탈을 통해 크레아토르로 가서 마왕의 하수인을 물리치고 타락한 정령들을 원래의 모습으로 돌려놓아주세요.");
         talkData2.Add(3, "");
 
