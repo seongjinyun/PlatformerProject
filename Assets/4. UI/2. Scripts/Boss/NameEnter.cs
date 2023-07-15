e20d46d688f17466e3ee9a43359d8321ccb4b1aa
SangMyeong5426 <tvn2398@gmail.com>
2023-07-15 17:39:31 +0900

    InputName

---
+++ Assets/4. UI/2. Scripts/Boss/NameEnter.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class NameEnter : MonoBehaviour
+{
+    public TMP_InputField playerNameInput;
+    private string playerName = null;
+
+    private void Awake()
+    {
+        playerName = playerNameInput.GetComponent<InputField>().text;
+    }
+
+    private void Update()
+    {
+    }
+
+    //¸¶¿ì½º
+    public void InputName()
+    {
+        playerName = playerNameInput.text;
+        PlayerPrefs.SetString("CurrentPlayerName", playerName);
+
+        LoadingSceneController.LoadScene("UI_Main");
+    }
+}
