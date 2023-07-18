c4219bc0baae997069b9c0b6c5b38bf928c7cb62
kh6206 <jkh6206@naver.com>
2023-07-18 21:07:00 +0900

    d

--- Assets/2. Player/02. Scripts/Unit.cs
+++ Assets/2. Player/02. Scripts/Unit.cs
@@ -27,8 +27,9 @@ namespace AllUnits
 
         public AudioClip[] clip_attacked; // 피격 사운드 
 
-        
-        
+        public GameObject Attacked_Effect; // 피격 이펙트
+
+
 
         private float damageTimer = 0f;
 
@@ -52,7 +53,8 @@ namespace AllUnits
         public void TakeDamage(int Monster_Damage) // 피격 
         {
 
-            
+            //GameObject Atk_Ef = Instantiate(Attacked_Effect, me.transform.position, me.transform.rotation);
+            //Destroy(Atk_Ef, 0.5f);
 
             //SfxManger.instance.SfxPlay("Monster_Attacked", clip_attacked[0]);
             if (clip_attacked.Length > 0)
